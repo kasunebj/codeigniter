37e9ec3ec3c001dfce9d33b0bef3af05911dbb75
kasunebj <30827556+kasunebj@users.noreply.github.com>
2018-07-11 09:05:43 +0530

    search and insert controller

--- Home.php
+++ Home.php
@@ -1,19 +1,32 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
 
-	
-	public function index()
-	{
-		$this->load->view('home');
-	}
-	public function login()
-	{
-		$this->load->view('login');
-	}
-	public function register()
-	{
-		$this->load->view('register');
-	}
+class Home extends CI_Controller
+{
+    public function index(){
+        $this->load->view('home');
+    }
+    public function add(){
+       $this->load->model('user');
+      $response=  $this->user->add();
+        if ($response){
+            $this->session->set_flashdata('msg', 'successful');
+            redirect('Home/index');
+        }
+    }
+
+    public function get(){
+        $this->load->model('user');
+        $result['a']=  $this->user->get();
+
+
+        $this->load->view('results_view', $result);
+
+    }
+    public function search(){
+        $this->load->model('user');
+        $result['b'] = $this->user->search();
+        $this->load->view('search', $result);
+    }
+
 }
